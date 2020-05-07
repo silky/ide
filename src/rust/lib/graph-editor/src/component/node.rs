1d439f248bb9380979ba690bd0b4b979736bbfb1
Michael Mauderer <michael.mauderer@enso.org>
2020-05-07 18:51:22 +0200

    Fix memory leak.

--- src/rust/lib/graph-editor/src/component/node.rs
+++ src/rust/lib/graph-editor/src/component/node.rs
@@ -229,7 +229,7 @@ impl Node {
 
     fn init(self) -> Self {
         // TODO implement dynamic layouting.
-        self.data.visualization_container.set_position(Vector3::new(-50.0,-100.0,0.0));
+        self.data.visualization_container.set_position(Vector3::new(-50.0,-150.0,0.0));
         self.add_child(&self.data.visualization_container);
 
         let network = &self.data.events.network;
